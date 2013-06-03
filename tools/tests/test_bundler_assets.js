07833de9ccc92b92e2335d9225db04114973c2f6
Emily Stark <emily@meteor.com>
2013-06-03 15:08:22 -0700

    Add test for static server assets packaged correctly

--- tools/tests/test_bundler_assets.js
+++ tools/tests/test_bundler_assets.js
@@ -1,11 +1,14 @@
 var appWithPublic = path.join(__dirname, 'app-with-public');
+var appWithPrivate = path.join(__dirname, 'app-with-private');
 
-lib = new library.Library({
-  localPackageDirs: [ path.join(files.getCurrentToolsDir(), 'packages') ]
-});
+// These tests make some assumptions about the structure of stars: that there
+// are client and server programs inside programs/.
 
 console.log("Bundle app with public/ directory");
 assert.doesNotThrow(function () {
+  var lib = new library.Library({
+    localPackageDirs: [ path.join(files.getCurrentToolsDir(), 'packages') ]
+  });
   var tmpOutputDir = tmpDir();
   var result = bundler.bundle(appWithPublic, tmpOutputDir, {
     nodeModulesMode: 'skip',
@@ -23,4 +26,45 @@ assert.doesNotThrow(function () {
                      "Test");
   assert.strictEqual(fs.readFileSync(nestedTxtPath, "utf8").trim(),
                      "Nested");
+});
+
+console.log("Bundle app with private/ directory and package asset");
+assert.doesNotThrow(function () {
+  var lib = new library.Library({
+    localPackageDirs: [ path.join(files.getCurrentToolsDir(), 'packages'),
+                      path.join(appWithPrivate, "packages") ]
+  });
+  var tmpOutputDir = tmpDir();
+  var result = bundler.bundle(appWithPrivate, tmpOutputDir, {
+    nodeModulesMode: 'skip',
+    library: lib,
+    releaseStamp: 'none'
+  });
+  var serverManifest = JSON.parse(
+    fs.readFileSync(
+      path.join(tmpOutputDir, "programs", "server",
+                "program.json")
+    )
+  );
+  var staticDir = path.join(tmpOutputDir, "programs",
+                            "server", serverManifest.static);
+  var testTxtPath = path.join(staticDir, "test.txt");
+  var nestedTxtPath = path.join(staticDir, "nested", "test.txt");
+
+  var packageTxtPath;
+  _.each(serverManifest.load, function (item) {
+    console.log(item.path);
+    if (item.path === "/packages/test-package.js") {
+      packageTxtPath = path.join(tmpOutputDir,
+                                 "programs", "server",
+                                 item.staticDir, "test-package.txt");
+    }
+  });
+  assert.strictEqual(result.errors, false, result.errors && result.errors[0]);
+  assert(fs.existsSync(testTxtPath));
+  assert(fs.existsSync(nestedTxtPath));
+  assert(fs.existsSync(packageTxtPath));
+  assert.strictEqual(fs.readFileSync(testTxtPath, "utf8").trim(), "Test");
+  assert.strictEqual(fs.readFileSync(nestedTxtPath, "utf8").trim(), "Nested");
+  assert.strictEqual(fs.readFileSync(packageTxtPath, "utf8").trim(), "Package");
 });
