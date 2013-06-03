4cb45794ae76c7481f4c96856d2645c7d3732e11
Emily Stark <emily@meteor.com>
2013-06-03 12:23:50 -0700

    Include private/ files in target's static/ dir

--- tools/bundler.js
+++ tools/bundler.js
@@ -684,11 +684,7 @@ _.extend(Target.prototype, {
         if (options.setUrl)
           f.setUrlFromRelPath(assetPath);
         if (options.setTargetPath)
-          f.setTargetPathFromRelPath(path.join(path.sep,
-                                               'static',
-                                               path.relative(rootDir,
-                                                             assetPath)));
-
+          f.setTargetPathFromRelPath(path.join('/static', assetPath));
         self.dependencyInfo.files[absPath] = f.hash();
         self.static.push(f);
       });
